9486b5380878e6919cb183c37ce77df6a731ba64
riccione <riccione@protonmail.com>
2023-02-03 23:21:13 -0500

    Add basic logic
    - read json file
    I took logic from process words

--- glacier_helper_python/helper.py
+++ glacier_helper_python/helper.py
@@ -1,9 +1,44 @@
-'''
+"""
 depends on aws console
 
-- pass as arg json file (glacier)
 - parse it
 - 2 possible ways:
     - generate html with all commands
     - interactively generate aws console commands for glacier
-'''
+"""
+import argparse
+from pathlib import Path
+import json
+
+
+def read_file(filename):
+    with open(filename, "r") as f:
+        data = json.load(f)
+    return data
+
+
+def is_file_exist(x):
+    if x is not None:
+        x = Path(x)
+        if x.is_file() and x.suffix == ".json":
+            return True
+    return False
+
+
+def parse(x):
+    data = read_file(x)
+    print(data)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename", help="path to json file", type=str)
+    args = parser.parse_args()
+    if is_file_exist(args.filename):
+        parse(args.filename)
+    else:
+        print("File not found. Please provide a valid path to json filename")
+
+
+if __name__ == "__main__":
+    main()
