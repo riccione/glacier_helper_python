a8eb75823c843fc45a1f27b2b1cec1b24388efa9
riccione <riccione@protonmail.com>
2023-02-03 23:05:33 -0500

    init

---
+++ glacier_helper_python/helper.py
@@ -0,0 +1,9 @@
+'''
+depends on aws console
+
+- pass as arg json file (glacier)
+- parse it
+- 2 possible ways:
+    - generate html with all commands
+    - interactively generate aws console commands for glacier
+'''
