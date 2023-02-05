18508dd9282d06137d9384b1bc52630c16bbba9b
riccione <riccione@protonmail.com>
2023-02-04 22:02:58 -0500

    Add fn to generate html
    There is a bug with copy fn - see cp js fn in the fn txt_to_html.
    cp fn does not do what it should!

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # glacier_helper_python
+
+## Usage
+
+`poetry run python glacier_helper_python/helper.py <path_to_json>`
--- glacier_helper_python/helper.py
+++ glacier_helper_python/helper.py
@@ -9,6 +9,8 @@ depends on aws console
 import argparse
 from pathlib import Path
 import json
+from datetime import datetime as dt
+from string import Template
 
 
 def read_file(filename):
@@ -25,9 +27,64 @@ def is_file_exist(x):
     return False
 
 
+def datetime_h(x):
+    y = dt.fromisoformat(x)
+    return f"{y.day}.{y.month}.{y.year} {y.hour}:{y.minute}"
+
+def size_h(x, suffix="B"):
+    for unit in ["", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"]:
+        if abs(x) < 1024.0:
+            return f"{x:3.1f}{unit}{suffix}"
+        x /= 1024.0
+    return f"{x:.1f}Yi{suffix}"
+
+def extract(xs: list) -> dict:
+    r = {}
+    for x in xs:
+        ys = json.loads(x["ArchiveDescription"])
+        t = []
+        t.append(f"{ys['Path']} | \
+            {size_h(x['Size'])} | \
+            {datetime_h(x['CreationDate'])}")
+        r[x['ArchiveId']] = t
+    return r
+
+def txt_to_html(xs: dict) -> str:
+    v = 'vault_name'
+    t = "<!DOCTYPE html><html><title>output.json to output.html</title><body>"
+    t += f"<h2>Glacier vault contains: {len(xs)} objects aka files:</h2>"
+    t1 = '''
+    <button onclick="cp('$id')">Initiate Job</button></br>
+    '''
+    for x, y in xs.items():
+        aws_initiate_job = f"aws glacier initiate-job --vault-name {v} "
+        aws_initiate_job += f"--account-id - --job-parameters "
+        aws_initiate_job += '{\"Type\":\"archive-retrieval\",\"Tier\":\"Bulk\",\"ArchiveId\":\"'
+        aws_initiate_job += x
+        aws_initiate_job += '\"}'
+        t += f"{y} | <input type='hidden' value='{aws_initiate_job}' id='{x}'>"
+        t += Template(t1).safe_substitute(id=x)
+    t += "<script>"
+    t += "function cp(y) {"
+    t += "let x = document.getElementById(y);"
+    t += "x.select();"
+    t += "x.setSelectionRange(0, 99999);"
+    t += "navigator.clipboard.writeText(x.value);"
+    t += "navigator.clipboard.writeText(x.value);"
+    t += "}"
+    t += "</script>"
+    t += "</body></html>"
+    return t
+
+def save_html(xs, y="output.html"):
+    ys = txt_to_html(xs)
+    with open(y, "w") as f:
+        f.write(ys)
+
 def parse(x):
     data = read_file(x)
-    print(data)
+    r = extract(data["ArchiveList"])
+    save_html(r)
 
 
 def main():
